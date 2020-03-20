5515cdfe9a0a33b67157c7bcc54584a5ee8e4e6a
Bruno Diorio <bruno.diorio@recargapay.com>
2020-03-20 19:19:59 -0300

    init commit

---
+++ src/main/java/com/example/cookbooktest/CookbookTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookbooktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookbookTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookbooktest/CookbookTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookbooktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
